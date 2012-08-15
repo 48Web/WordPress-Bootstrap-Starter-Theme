b13343856a1588694c90113e1a82ce8499f20dd2
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2012-08-15 12:59:47 -0500

    archive page

--- archive.php
+++ archive.php
@@ -1,16 +1,18 @@
 <?php /* --[ Archive Page Template ] -- */ ?>
 <?php get_header(); ?>
 <div class="row<?php if (IS_RESPONSIVE) echo '-fluid' ?>">
-	<div id="content" class="span12">
-	    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+    <div id="content" class="span9">
+        <?php if (have_posts()): while (have_posts()): the_post(); ?>
             <div class="post">
                 <h1><a href="<?php the_permalink() ?>" rel="permalink"><?php the_title(); ?></a></h1>
                 <div class="post-content">
-                    <?php echo the_excerpt(); ?>
+                    <?php echo the_content(); ?>
                 </div>
-                <?php endif; ?>
-		    </div><!--#end post-->
+            </div><!--#end post-->
         <?php endwhile; endif; ?>
-	</div><!--#end content -->
+    </div><!--#end content -->
+    <div class="span3">
+        <?php get_sidebar(); ?>
+    </div><!--#end sidebar -->
 </div>
 <?php get_footer(); ?>
